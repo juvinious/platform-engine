eb4c15c288e0ad331007ab787445881578709b4d
juvinious <juvinious@users.sourceforge.net>
2011-01-10 22:22:27 +0000

    Move platformer testing to game class.

---
+++ game.h
@@ -0,0 +1,36 @@
+#ifndef platformer_game_h
+#define platformer_game_h
+
+#include <vector>
+
+#include "util/input/input-map.h"
+
+class Token;
+
+/*! Platformers game class this is for testing, will be expanded or changed later */
+
+namespace Platformer{
+    
+class World;
+        
+class Game{
+    public:
+	Game(const std::string &);
+	virtual ~Game();
+	virtual void run();
+	
+    protected:
+	std::vector < Platformer::World *> worlds;
+	//! keys
+	enum Keys{
+	    Up=0,
+	    Down,
+	    Left,
+	    Right,
+	    Esc,
+	};
+	
+	InputMap<Keys> input;
+};
+}
+#endif
