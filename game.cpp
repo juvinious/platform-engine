7388268a3cd9fbb5d1e7e74b1e23e68855a71f01
juvinious <juvinious@users.sourceforge.net>
2011-04-14 20:01:14 +0000

    Begin using standard loop for rendering world.

--- game.cpp
+++ game.cpp
@@ -5,6 +5,7 @@
 
 #include "util/bitmap.h"
 #include "util/debug.h"
+#include "util/events.h"
 #include "util/font.h"
 #include "util/init.h"
 #include "util/input/input-map.h"
@@ -71,6 +72,95 @@ Game::~Game(){
 void Game::run(){
     // NOTE Testing purposes only
     
+    class Logic: public Util::Logic {
+    public:
+        Logic(InputMap<Keys> & input, std::vector < Platformer::World *> & worlds, double & counter):
+        is_done(false),
+        input(input),
+        worlds(worlds),
+        counter(counter){
+        }
+
+        bool is_done;
+	InputMap<Keys> & input;
+	std::vector < Platformer::World *> & worlds;
+	double & counter;
+        
+	bool done(){
+            return is_done;
+        }
+
+	void run(){
+	    
+	    // FIXME figure out how many worlds... etc
+            InputManager::poll();
+	    vector<InputMap<Keys>::InputEvent> out = InputManager::getEvents(input);
+	    for (vector<InputMap<Keys>::InputEvent>::iterator it = out.begin(); it != out.end(); it++){
+		const InputMap<Keys>::InputEvent & event = *it;
+		if (event.enabled){
+		    if (event.out == Esc){
+			is_done = true;
+		    }
+		    if (event.out == Up){
+			worlds[0]->moveCamera(0,-5);
+		    }
+		    if (event.out == Down){
+			worlds[0]->moveCamera(0,5);
+		    }
+		    if (event.out == Left){
+			worlds[0]->moveCamera(-5,0);
+		    }
+		    if (event.out == Right){
+			worlds[0]->moveCamera(5,0);
+		    }
+		}
+	    }
+	    
+	    if (Global::speed_counter3 > 0){
+		counter += Global::speed_counter3 * Global::LOGIC_MULTIPLIER;
+		
+		while (counter >= 1.0){
+		    counter -= 1;
+		    worlds[0]->act();
+		}
+
+		Global::speed_counter3 = 0;
+	    }
+		
+        }
+
+        double ticks(double system){
+            return counter;
+        }
+    };
+
+    class Draw: public Util::Draw {
+    public:
+        Draw(std::vector < Platformer::World * > & worlds, const Graphics::Bitmap & buffer, const Logic & logic):
+        worlds(worlds),
+        buffer(buffer),
+        logic(logic){
+        }
+        
+        std::vector < Platformer::World * > & worlds;
+
+        const Graphics::Bitmap & buffer;
+        
+	const Logic & logic;
+
+        void draw(){
+	    
+	    if (Global::speed_counter3 > 0){
+		worlds[0]->draw(buffer);
+		ostringstream info;
+		info << "Camera Info - X: " << worlds[0]->getCamera(0).getX() << " Y: " << worlds[0]->getCamera(0).getY();
+		Font::getDefaultFont().printf( 10, 10, Graphics::makeColor(255,255,255), buffer, info.str(), 0);
+		buffer.BlitToScreen();
+	    }
+        }
+    };
+    
+    // set input
     input.set(Keyboard::Key_ESC, 0, true, Esc);
     input.set(Keyboard::Key_UP, 0, true, Up);
     input.set(Keyboard::Key_DOWN, 0, true, Down);
@@ -78,13 +168,17 @@ void Game::run(){
     input.set(Keyboard::Key_RIGHT, 0, true, Right);
     
     Global::speed_counter3 = 0;
-    double min_y = GFX_Y;
-
+    double counter = 0;
+    
     // FIXME change this later as the actual resolution is in the world configuration
     Graphics::Bitmap tmp(640, 480);
+
+    Logic logic(input, worlds, counter);
+    Draw draw(worlds, tmp, logic);
+
+    Util::standardLoop(logic, draw);
     
-    const Font & vFont = Font::getDefaultFont();
-    
+#if 0    
     bool quit = false;
     double think = 0;
     while (!quit){
@@ -138,4 +232,6 @@ void Game::run(){
     }
 
     InputManager::waitForRelease(input, Esc);
+#endif
+
 }
