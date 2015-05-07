8db72ca9b1f3ba86f939e49a210fdd1827f571a0
juvinious <juvinious@gmail.com>
2015-05-07 12:40:08 -0400

    Added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <r-tech1/init.h>
+#include <r-tech1/debug.h>
+#include "argument.h"
+
+int main(int argc, char ** argv){
+    
+    Global::InitConditions conditions;
+    
+    Global::debug(0) << "Starting up r-tech1..." << std::endl;
+    
+    Global::init(conditions);
+
+
+    
+    Global::debug(0) << "Done! Exiting..." << std::endl;
+    
+    Global::close();
+    
+    return 0;
+}
